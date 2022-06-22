1a1a769107aa88bf39d16ec7106ed8e6731bb756
Givaldo Neto <valdingivs@outlook.com>
2022-06-21 22:29:13 -0300

    feat: add more routes and refactoring models

--- main.py
+++ main.py
@@ -35,12 +35,22 @@ class User(BaseModel):
 
 
 class Author(BaseModel):
-    id: int
-    user_id: int
+    id: int = None
     name: str
 
     class Config:
         orm_mode = True
+
+
+class Paper(BaseModel):
+    id: int = None
+    author_id: int
+    title: str
+    sumary: str
+
+    class Config:
+        orm_mode = True
+
 
 db = SessionLocal()
 
@@ -67,13 +77,12 @@ def create_user(user: User):
         office=user.office
     )
 
-
     db_user = db.query(models.User).filter_by(email=new_user.email).count()
 
     if db_user >= 1:
         raise HTTPException(status_code=400, detail="Email already exists")
 
-    token = signJWT(user.email)
+    token = signJWT(user.email, user.office)
 
     if token:
         db.add(new_user)
@@ -95,9 +104,48 @@ def check_user(data: UserLogin):
 @app.post("/users/login", tags=["user"])
 def user_login(user: UserLogin = Body(default=None)):
     if check_user(user):
-        return signJWT(user.email)
+        get_user_office = db.query(models.User).filter(models.User.email == user.email).all()
+        return signJWT(user.email, get_user_office[0].office)
     else:
         raise HTTPException(status_code=401, detail="Invalid Email or password")
 
+@app.post("/authors", dependencies=[Depends(jwtBearer())], response_model=Author, status_code=status.HTTP_201_CREATED, tags=["author"])
+def create_author(author: Author):
+    new_author = models.Author(
+        name=author.name
+    )
 
+    db.add(new_author)
+    db.commit()
+    return new_author
+
+
+@app.get('/authors', dependencies=[Depends(jwtBearer())], response_model=List[Author], status_code=200)
+def get_all_authors():
+    authors = db.query(models.Author).all()
+
+    return authors
+
+
+@app.post("/papers", dependencies=[Depends(jwtBearer())], response_model=Paper, status_code=status.HTTP_201_CREATED, tags=["paper"])
+def create_paper(paper: Paper):
+    new_paper = models.Paper(
+        author_id=paper.author_id,
+        title=paper.title,
+        sumary=paper.sumary
+    )
+
+    find_author_id = db.query(models.Author).filter(models.User.id == paper.author_id).count()
+    if not find_author_id:
+        raise HTTPException(status_code=404, detail="Author doesn't exist.")
+
+    db.add(new_paper)
+    db.commit()
+    return new_paper
+
+@app.get('/papers', dependencies=[Depends(jwtBearer())], response_model=List[Paper], status_code=200)
+def get_all_papers():
+    papers = db.query(models.Paper).all()
+
+    return papers
 
--- models.py
+++ models.py
@@ -2,37 +2,38 @@ from database import Base
 from sqlalchemy import String, Boolean, Integer, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class User(Base):
     __tablename__='users'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(255), nullable=False)
     email = Column(String(255), nullable=False, unique=True)
     password = Column(String(255), nullable=False)
     office = Column(String(255))
-    author = relationship("Author")
-    paper = relationship("Paper")
 
     def __repr__(self):
-        return f"<User name={self.name} email={self.email} password={self.password} isSuperUser={self.office}"
+        return f"<User id={self.id} name={self.name} email={self.email} password={self.password} isSuperUser={self.office}"
 
 
 class Author(Base):
     __tablename__ = 'authors'
     id = Column(Integer, primary_key=True)
-    fk_user_id = Column(Integer, ForeignKey("users.id"))
-    name = Column(String(255), nullable=False)
-    paper = relationship("Paper")
+    name = Column(String(255), nullable=False, unique=True)
+    paper = relationship("Paper", back_populates="owner")
 
     def __repr__(self):
-        return f"<User name={self.name} email={self.fk_user_id}"
+        return f"<User id={self.id} name={self.name}"
+
 
 class Paper(Base):
     __tablename__ = 'papers'
     id = Column(Integer, primary_key=True)
-    fk_user_id = Column(Integer, ForeignKey("users.id"))
-    fk_author_id = Column(Integer, ForeignKey("authors.id"))
+    author_id = Column(Integer, ForeignKey("authors.id"))
     title = Column(String(255), nullable=False)
     sumary = Column(String(255), nullable=False)
 
+    owner = relationship("Author", back_populates="paper")
 
+    def __repr__(self):
+        return f"<User id={self.id} name={self.title}"
 
