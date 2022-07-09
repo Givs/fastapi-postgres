fd83c0b225bb44678b24fcb432f231645e11acff
Givaldo Neto <valdingivs@outlook.com>
2022-07-09 20:49:27 -0300

    Revert "feat: add expiry logic"
    This reverts commit 641f06fc1409d1d13703ffd07727885c22e1944b.

--- auth/jwt_bearer.py
+++ auth/jwt_bearer.py
@@ -1,7 +1,7 @@
 from fastapi import Request, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from .jwt_handler import verify_token
-from errors.error_instance import error_instance
+from .jwt_handler import decodeJWT
+
 
 class jwtBearer(HTTPBearer):
     def __int__(self, auto_Error=True):
@@ -13,17 +13,17 @@ class jwtBearer(HTTPBearer):
         request_route = request.url.components.path
         if credentials:
             if not credentials.scheme == "Bearer":
-                error_instance(403, "Inválid or Expired Token!")
+                raise HTTPException(status_code=403, detail="Inválid or Expired Token!")
             permition = self.verify_jwt(credentials.credentials, request_method, request_route)
             if not permition:
-                error_instance(403, "Inválid or Expired Token!")
+                raise HTTPException(status_code=403, detail="Inválid or Expired Token!")
             return credentials.credentials
         else:
-            error_instance(403, "Inválid or Expired Token!")
+            raise HTTPException(status_code=403, detail="Inválid or Expired Token!")
 
     def verify_jwt(self, jwtoken, request_method, request_route):
         is_token_valid = False
-        payload = verify_token(jwtoken, request_method, request_route)
+        payload = decodeJWT(jwtoken, request_method, request_route)
         if payload:
             is_token_valid = True
         return is_token_valid
--- auth/jwt_handler.py
+++ auth/jwt_handler.py
@@ -2,7 +2,7 @@
 
 # set expiration limit for the tokens
 import time
-import datetime
+
 # resposible for encondig and decogind generated token
 import jwt
 
@@ -11,7 +11,6 @@ from decouple import config
 
 JWT_SECRET = config("secret")
 JWT_ALGORITHM = config("algorithm")
-NUM_SECONDS_IN_A_MIN = 60
 
 #User can be logged in for 20 minutes
 ACCESS_TOKEN_EXPIRE_SECONDS = 1200
@@ -34,58 +33,17 @@ def signJWT(userID: str, userOffice: str):
     return token_reponse(token)
 
 
-def decode_jwt(token: str):
+def decodeJWT(token: str, request_method, request_route):
+    #TODO tirar essa verificação de rotas admin daqui
+    admin_routes = ['/authors/', '/papers/']
+    admin_methods = ['POST', 'PATH', 'DELETE']
     try:
-        return jwt.decode(token, JWT_SECRET, algorithm=JWT_ALGORITHM)
-    except Exception as e:
-        return {'error': str(e)}
-
-
-def verify_user_office_can_access_endpoint(office, request_method, request_route):
-    ADMIN_ROUTES = ['/authors/', '/papers/']
-    ADMIN_METHODS = ['POST', 'PATH', 'DELETE']
-
-    if request_method in ADMIN_METHODS and request_route in ADMIN_ROUTES:
-        return True if office == 'Admin' else False
-    return True
-
-
-def verify_token(token: str, request_method, request_route):
-    try:
-        decode_token = decode_jwt(token)
+        decode_token = jwt.decode(token, JWT_SECRET, algorithm=JWT_ALGORITHM)
         if decode_token:
             office = decode_token['userOffice']
-            user_access = verify_user_office_can_access_endpoint(office, request_method, request_route)
-            if not user_access:
-                return {}
-        return decode_token if decode_token['expiry'] >= time.time() else None
+            if request_method in admin_methods and request_route in admin_routes:
+                if office != 'Admin':
+                    return {}
+            return decode_token if decode_token['expiry'] >= time.time() else None
     except:
         return {}
-
-
-def verify_minutes(minutes):
-    return 's' if minutes > 1 else ''
-
-
-def personalize_message(life_time_in_minutes, is_minute_singular_or_plural):
-    message = 'Your token will expiry in less than 1 minute.' if life_time_in_minutes < 0 \
-              else f'Your token will expire in {life_time_in_minutes} minute{is_minute_singular_or_plural}.'
-
-    return {'message': message}
-
-
-def get_expiry_token(request):
-    get_jwt_from_request = request.headers.raw[3][1].decode('utf-8').split(' ')[1]
-
-    try:
-        decoded_token = decode_jwt(get_jwt_from_request)
-    except Exception as e:
-        return {'error': str(e)}
-
-    time_to_end_session = datetime.datetime.fromtimestamp(decoded_token['expiry'])
-    now = datetime.datetime.now()
-    life_time_in_minutes = int(((time_to_end_session - now).total_seconds()) / NUM_SECONDS_IN_A_MIN)
-
-    is_minute_singular_or_plural = verify_minutes(life_time_in_minutes)
-
-    return personalize_message(life_time_in_minutes, is_minute_singular_or_plural)
